8835db618a435b75f21e38d4a80784b485358142
KalyaniRangani <kalyanireddyrangani@gmail.com>
2026-06-30 11:01:10 +0530

    Create cicd.groovy

---
+++ vars/cicd.groovy
@@ -0,0 +1,16 @@
+def contdown(repo)
+{
+  git 'http://github.com/KalyaniRangani/${repo}.git'
+}
+def contbuild()
+{
+  sh 'mvn package'
+}
+def contdeploy(jobname,ipadd,contextpath)
+{
+  sh 'scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ipadd}:/var/lib/tomcat10/webapps/testappfuc.war'
+}
+def conttest(jobname)
+{
+  sh 'scp /var/lib/jenkins/workspace/${jobname}/testing.jar
+}
